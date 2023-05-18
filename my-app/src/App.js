835763efa41658c2bdd15bcb09c71f77b55025cc
Ashu <ashutosh123asd@gmail.com>
2023-05-18 13:29:51 +0530

    Task 9

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-import ExpenseItems from './components/ExpenseItems';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      LocationOfExpenditure:'Delhi',
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2',
+      title: 'New TV',
+      amount: 799.49,
+      LocationOfExpenditure:'nagpur',
+      date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      LocationOfExpenditure:'punjab',
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      LocationOfExpenditure:'mumbai',
+      date: new Date(2021, 5, 12),
+    },
+  ];
+  const list=[]
+  expenses.forEach((element) => {
+    list.push(
+      <ExpenseItem
+      key={element.id}
+    title={element.title}
+    amount={element.amount}
+    date={element.date}
+    LocationOfExpenditure={element.LocationOfExpenditure}
+  ></ExpenseItem>
+    )
+  })
+  
+
   return (
-    <div >
-        <h2>ExpenseItems</h2>
-        <ExpenseItems></ExpenseItems>
+    <div>
+      <h2>Let's get started!</h2>
+     {list}
     </div>
   );
 }
