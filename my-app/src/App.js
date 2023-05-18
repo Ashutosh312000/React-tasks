09523c6ec4864759ba779dc922976ac83f253dbd
Ashu <ashutosh123asd@gmail.com>
2023-05-18 12:15:46 +0530

    Task 7

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import ExpenseItems from './components/ExpenseItems';
+
+function App() {
+  return (
+    <div >
+        <h2>ExpenseItems</h2>
+        <ExpenseItems></ExpenseItems>
+    </div>
+  );
+}
+
+export default App;
