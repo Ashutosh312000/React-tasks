63e4529bbf5f75cb7da81a61043ee6ec0010f7ef
Ashu <ashutosh123asd@gmail.com>
2023-05-25 09:50:05 +0530

    Task 2

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,12 @@
 import ExpenseDate from './ExpenseDate';
 import Card from './Card';
 import './ExpenseItem.css';
+import React, {useState} from 'react'
 
 function ExpenseItem(props) {
-
+  const [amount,setAmount]=useState(props.amount)
   function clickHandler(){
-    console.log('delete');
+    setAmount('100');
   }
 
   return (
@@ -13,9 +14,9 @@ function ExpenseItem(props) {
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
+        <div className='expense-item__price'>${amount}</div>
       </div>
-      <button onClick={clickHandler} >Delete</button>
+      <button onClick={clickHandler} >Change amount</button>
     </Card>
   );
 }
